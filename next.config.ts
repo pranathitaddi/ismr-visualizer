6adbd358865ebdb49aa93af8a0d3466e47bc8fee
Pranathi Taddi <pranathhitaddi@gmail.com>
2026-04-30 02:47:36 +0530

    Build Changes

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ module.exports = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://127.0.0.1:5328/:path*',
+        destination: process.env.NODE_ENV === 'development'
+                ? 'http://127.0.0.1:8000/api/:path*'
+                : '/api/',
       },
     ]
   },
