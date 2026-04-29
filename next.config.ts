7b970f0c40f83ff6ff3730e09a6f562af25ca918
Pranathi Taddi <pranathhitaddi@gmail.com>
2026-04-30 03:49:41 +0530

    changed api

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,14 @@
-module.exports = {
+const nextConfig = {
   async rewrites() {
     return [
       {
         source: '/api/:path*',
         destination: process.env.NODE_ENV === 'development'
-                ? 'http://127.0.0.1:8000/api/:path*'
-                : '/api/',
+          ? 'http://127.0.0.1:8000/:path*'
+          : `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ]
   },
 }
+
+module.exports = nextConfig
